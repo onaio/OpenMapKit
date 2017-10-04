136934ac4a79979f9359729ff4b5f1403488a3ca
Jason Rogena <jasonrogena@gmail.com>
2017-10-04 14:26:41 +0300

    Changing Admin Password
    Add the ability to change the admin password.
    
    Fixes issue #68
    
    Signed-off-by: Jason Rogena <jasonrogena@gmail.com>

---
+++ app/src/main/java/org/redcross/openmapkit/HashingUtils.java
@@ -0,0 +1,33 @@
+package org.redcross.openmapkit;
+
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * Created by jrogena on 10/4/17.
+ */
+
+public class HashingUtils {
+    private static String convertToHex(byte[] data) {
+        StringBuilder buf = new StringBuilder();
+        for (byte b : data) {
+            int halfbyte = (b >>> 4) & 0x0F;
+            int two_halfs = 0;
+            do {
+                buf.append((0 <= halfbyte) && (halfbyte <= 9) ? (char) ('0' + halfbyte) : (char) ('a' + (halfbyte - 10)));
+                halfbyte = b & 0x0F;
+            } while (two_halfs++ < 1);
+        }
+        return buf.toString();
+    }
+
+    public static String getSHA1Hash(String text)
+            throws NoSuchAlgorithmException, UnsupportedEncodingException {
+        MessageDigest md = MessageDigest.getInstance("SHA-1");
+        byte[] textBytes = text.getBytes("iso-8859-1");
+        md.update(textBytes, 0, textBytes.length);
+        byte[] sha1hash = md.digest();
+        return convertToHex(sha1hash);
+    }
+}
