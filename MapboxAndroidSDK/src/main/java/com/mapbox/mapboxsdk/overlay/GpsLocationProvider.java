08af903db63e00185e35908be9f96f2c35b59dfb
Jason Rogena <jasonrogena@gmail.com>
2017-10-05 19:22:15 +0300

    GoogleApiClient for Location Updates
    Add support for using GoogleApiClient for getting location updates
    
    Part of fix for issue #32
    
    Signed-off-by: Jason Rogena <jasonrogena@gmail.com>

--- MapboxAndroidSDK/src/main/java/com/mapbox/mapboxsdk/overlay/GpsLocationProvider.java
+++ MapboxAndroidSDK/src/main/java/com/mapbox/mapboxsdk/overlay/GpsLocationProvider.java
@@ -6,14 +6,30 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.util.Log;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationAvailability;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
 import com.mapbox.mapboxsdk.util.NetworkLocationIgnorer;
 
 import java.util.ArrayList;
 import java.util.Calendar;
 
-public class GpsLocationProvider implements LocationListener {
+public class GpsLocationProvider implements LocationListener, GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        com.google.android.gms.location.LocationListener {
+    public static enum LocationStrategy {
+        LOCATION_MANAGER,
+        GOOGLE_PLAY_SERVICES
+    }
+
     private final LocationManager mLocationManager;
     private Location mLocation;
 
@@ -22,8 +38,14 @@ public class GpsLocationProvider implements LocationListener {
     private float mLocationUpdateMinDistance = 0.0f;
     private final NetworkLocationIgnorer mIgnorer = new NetworkLocationIgnorer();
     private final ArrayList<LocationListener> locationListeners;
+    private final Context context;
+    private final LocationStrategy locationStrategy;
+    private GoogleApiClient googleApiClient;
+    private LocationCallback locationCallback;
 
-    public GpsLocationProvider(Context context) {
+    public GpsLocationProvider(Context context, LocationStrategy locationStrategy) {
+        this.context = context;
+        this.locationStrategy = locationStrategy;
         mLocationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
         locationListeners = new ArrayList<>();
     }
@@ -79,6 +101,17 @@ public class GpsLocationProvider implements LocationListener {
     public boolean startLocationProvider(UserLocationOverlay myLocationConsumer) {
         mMyLocationConsumer = myLocationConsumer;
         boolean result = false;
+        if (locationStrategy.equals(LocationStrategy.LOCATION_MANAGER)) {
+            result = connectLocationManager(myLocationConsumer);
+        } else if (locationStrategy.equals(LocationStrategy.GOOGLE_PLAY_SERVICES)) {
+            result = connectGoogleApiClient();
+        }
+
+        return result;
+    }
+
+    private boolean connectLocationManager(UserLocationOverlay mMyLocationConsumer) {
+        boolean result = false;
         for (final String provider : mLocationManager.getProviders(true)) {
             if (LocationManager.GPS_PROVIDER.equals(provider)
                     || LocationManager.PASSIVE_PROVIDER.equals(provider)
@@ -94,12 +127,55 @@ public class GpsLocationProvider implements LocationListener {
                         mLocationUpdateMinDistance, this);
             }
         }
+
         return result;
+    }
+
+    private boolean connectGoogleApiClient() {
+        if (googleApiClient == null) {
+            Log.d("google_api", "Connecting to google api");
+
+            locationCallback = new LocationCallback() {
+                @Override
+                public void onLocationResult(LocationResult result) {
+                    super.onLocationResult(result);
+
+                }
+
+                @Override
+                public void onLocationAvailability(LocationAvailability locationAvailability) {
+                    super.onLocationAvailability(locationAvailability);
+                }
+            };
+            googleApiClient = new GoogleApiClient.Builder(context)
+                    .addApi(LocationServices.API)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .build();
+        }
+        googleApiClient.connect();
+
+        return true;
+    }
+
+    private void disconnectGoogleApiClient() {
+        if (googleApiClient != null) {
+            Log.d("google_api", "Disconnecting from google api");
+            googleApiClient.disconnect();
+        }
+    }
+
+    private void disconnectLocationManager() {
+        mLocationManager.removeUpdates(this);
     }
 
     public void stopLocationProvider() {
         mMyLocationConsumer = null;
-        mLocationManager.removeUpdates(this);
+        if (locationStrategy.equals(LocationStrategy.LOCATION_MANAGER)) {
+            disconnectLocationManager();
+        } else if (locationStrategy.equals(LocationStrategy.GOOGLE_PLAY_SERVICES)) {
+            disconnectGoogleApiClient();
+        }
     }
 
     public Location getLastKnownLocation() {
@@ -112,6 +188,11 @@ public class GpsLocationProvider implements LocationListener {
 
     @Override
     public void onLocationChanged(final Location location) {
+        Log.d("google_api", "Location changed to "+location.getLatitude() + " " + location.getLongitude());
+        locationChanged(location);
+    }
+
+    private void locationChanged(Location location) {
         // ignore temporary non-gps fix
         if (mIgnorer.shouldIgnore(location.getProvider(), System.currentTimeMillis())) {
             return;
@@ -150,5 +231,49 @@ public class GpsLocationProvider implements LocationListener {
 
     public LocationManager getLocationManager() {
         return mLocationManager;
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        Log.d("google_api", "Connected to api");
+        LocationRequest mLocationRequest=new LocationRequest();
+
+        mLocationRequest.setInterval(10000);
+        mLocationRequest.setFastestInterval(5000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        LocationServices.FusedLocationApi.requestLocationUpdates(googleApiClient, mLocationRequest, this);
+
+        if (mMyLocationConsumer != null) {
+            mLocation = LocationServices.FusedLocationApi.getLastLocation(googleApiClient);
+            if (mLocation != null) {
+                Log.d("google_api", "Last location is " + mLocation.getLatitude() + " " + mLocation.getLongitude());
+                mMyLocationConsumer.onLocationChanged(mLocation, this);
+            } else {
+                Log.d("google_api", "Last location is null");
+            }
+        } else {
+            Log.d("google_api", "Location consumer is null");
+        }
+
+        for(int i = 0; i < locationListeners.size(); i++) {
+            locationListeners.get(i).onProviderDisabled(null);
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int status) {
+        Log.d("google_api", "Connection to google api suspended");
+        for(int i = 0; i < locationListeners.size(); i++) {
+            locationListeners.get(i).onStatusChanged(null, status, null);
+        }
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.d("google_api", "Connection to google api failed");
+        for(int i = 0; i < locationListeners.size(); i++) {
+            locationListeners.get(i).onProviderDisabled(null);
+        }
     }
 }
